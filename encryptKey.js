f1ae896d7a8fc9ca9922dce266e5af77f80dce9f
Biliaminu Abdulfarid <faridosky99@gmail.com>
2024-03-30 01:06:12 -0700

    Create encryptKey.js

---
+++ encryptKey.js
@@ -0,0 +1,28 @@
+```javascript
+const ethers = require("ethers");
+const fs = require("fs-extra");
+require("dotenv").config();
+
+async function main() {
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+    
+    const encryptedJsonKey = await wallet.encrypt(
+        process.env.PRIVATE_KEY_PASSWORD,
+        process.env.PRIVATE_KEY
+    );
+    // In later version (^6.2.3 as of this commit) of ethers.js, PRIVATE_KEY is inferred from wallet, so there is no need to 
+    // pass private key again. 
+    //     const encryptedJsonKey = await wallet.encrypt(
+    //         process.env.PRIVATE_KEY_PASSWORD,
+    //  );
+    console.log(encryptedJsonKey);
+    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+```
